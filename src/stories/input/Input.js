a427c1aadd571d14d9c2a1a98e902ada4a25c6dd
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2024-01-04 15:07:50 +0300

    add input with button

--- src/stories/input/Input.js
+++ src/stories/input/Input.js
@@ -1,17 +1,35 @@
 export const Input = (args) => {
   const input = document.createElement('label')
-  input.innerHTML = `
-    <input
-      name=${args.name}
-      class="input__field"
-      type=${args.type}
-      ${args.disabled ? 'disabled' : ''}
-      placeholder=${args.placeholder}
-    >
-    <span class="input__label">${args.label}</span>
-  `
 
-  input.className = ['input'].join(' ')
+  const field = document.createElement('input')
+  field.classList.add('input__field')
+  field.name = args.name
+  field.type = args.type
+  field.disabled = args.disabled
+  field.placeholder = args.placeholder
+
+  const label = document.createElement('span')
+  label.classList.add('input__label')
+  label.textContent = args.label
+
+  input.appendChild(field)
+  input.appendChild(label)
+
+  if (args.variant === 'with-button') {
+    const button = document.createElement('button')
+    button.classList.add('input__button')
+    button.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+      </svg>
+    `
+
+    input.appendChild(button)
+  }
+
+  const variant = args.variant ? `input--${args.variant}` : ''
+
+  input.className = ['input', variant].join(' ')
 
   return input
 }
--- src/stories/input/Input.stories.js
+++ src/stories/input/Input.stories.js
@@ -4,13 +4,27 @@ import './input.scss'
 export default {
   title: 'Form/Input',
   component: Input,
+  argTypes: {
+    variant: {
+      options: ['with-button'],
+      control: { type: 'radio' }
+    }
+  },
   args: {
     type: 'text',
     name: 'name',
-    label: 'Enter name',
-    placeholder: 'Enter name',
+    label: 'Name',
+    placeholder: 'Name',
     disabled: false
   }
 }
 
 export const Default = {}
+
+export const WithButton = {
+  args: {
+    variant: 'with-button',
+    label: 'Copy',
+    placeholder: 'Copy'
+  }
+}
