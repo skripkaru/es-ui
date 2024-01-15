2436ca512b6842e8d0d07a59487aeb0c6db54b4e
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2024-01-15 18:03:47 +0300

    update typography

--- src/stories/text/Text.js
+++ src/stories/text/Text.js
@@ -2,11 +2,11 @@ export const Text = () => {
   const text = document.createElement('div')
 
   text.innerHTML = `
-    <p class="text text--xl">Text XL</p>
-    <p class="text text--l">Text L</p>
-    <p class="text text--m">Text M</p>
-    <p class="text text--s">Text S</p>
-    <p class="text text--xs">Text XS</p>`
+    <p class="text-xl">Text XL</p>
+    <p class="text-l">Text L</p>
+    <p class="text-m">Text M</p>
+    <p class="text-s">Text S</p>
+    <p class="text-xs">Text XS</p>`
 
   return text
 }
--- src/stories/title/Title.js
+++ src/stories/title/Title.js
@@ -2,10 +2,10 @@ export const Title = () => {
   const title = document.createElement('div')
 
   title.innerHTML = `
-    <h1 class="title title--xl">Title XL</h1>
-    <h2 class="title title--l">Title L</h2>
-    <h3 class="title title--m">Title M</h3>
-    <h4 class="title title--s">Title S</h4>`
+    <h1 class="h1">Title XL</h1>
+    <h2 class="h2">Title L</h2>
+    <h3 class="h3">Title M</h3>
+    <h4 class="h4">Title S</h4>`
 
   return title
 }
