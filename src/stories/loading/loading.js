da39e58332c841ec3857e278a486bc2c354fdb73
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2024-03-28 20:25:45 +0300

    added loading

---
+++ src/stories/loading/loading.js
@@ -0,0 +1,13 @@
+export const createLoading = () => {
+  const loading = document.createElement('div')
+
+  loading.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+    </svg>
+  `
+
+  loading.className = ['loading'].join(' ')
+
+  return loading
+}
---
+++ src/stories/loading/loading.stories.js
@@ -0,0 +1,9 @@
+import { createLoading as Loading } from './loading'
+import './loading.scss'
+
+export default {
+  title: 'Loading',
+  component: Loading
+}
+
+export const Default = {}
